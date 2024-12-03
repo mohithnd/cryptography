100a40ba235fb5b2fb9a40da7a552ec773608632
Mohit Agrawal <mohitagrawalhnd2003@gmail.com>
2024-12-03 13:23:34 +0530

    Modified Cease Cipher Algorithm

--- ciphers/ceaser_cipher.cpp
+++ ciphers/ceaser_cipher.cpp
@@ -1,40 +1,55 @@
 #include <iostream>
 using namespace std;
 
-string ceaser(string plain, int shift_key)
+string ceaser_cipher(string plain, int shift_key)
 {
+    string cipher_text;
     int n = plain.size();
     for (int i = 0; i < n; i++)
     {
         char ch = plain[i];
         if (ch >= 'A' && ch <= 'Z')
         {
-            int digit = ch - 'A';
-            digit = (digit + shift_key) % 26;
-            plain[i] = digit + 'A';
+            ch = (ch - 'A' + shift_key) % 26 + 'A';
         }
         else if (ch >= 'a' && ch <= 'z')
         {
-            int digit = ch - 'a';
-            digit = (digit + shift_key) % 26;
-            plain[i] = digit + 'a';
+            ch = (ch - 'a' + shift_key) % 26 + 'a';
         }
-        else
-        {
-            plain[i] = ch;
-        }
+        cipher_text += ch;
     }
-    return plain;
+    return cipher_text;
+}
+
+string ceaser_de_cipher(string cipher, int shift_key)
+{
+    string plain_text;
+    int n = cipher.size();
+    for (int i = 0; i < n; i++)
+    {
+        char ch = cipher[i];
+        if (ch >= 'A' && ch <= 'Z')
+        {
+            ch = (ch - 'A' - shift_key + 26) % 26 + 'A';
+        }
+        else if (ch >= 'a' && ch <= 'z')
+        {
+            ch = (ch - 'a' - shift_key + 26) % 26 + 'a';
+        }
+        plain_text += ch;
+    }
+    return plain_text;
 }
 
 int main()
 {
     string plain_text = "A B C D E F G H I J K L M N O P Q R S T U V W X Y Z";
-    int shift_key;
-    cout << "Enter Key: ";
-    cin >> shift_key;
-    string cipher_text = ceaser(plain_text, shift_key);
     cout << plain_text << endl;
+
+    string cipher_text = ceaser_cipher(plain_text, 3);
     cout << cipher_text << endl;
+
+    string de_cipher_text = ceaser_de_cipher(cipher_text, 3);
+    cout << de_cipher_text << endl;
     return 0;
 }
